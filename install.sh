7a87dc8d7df94bbae81d75d325f4ad754efa071b
Pharos <168114080+Pharosw@users.noreply.github.com>
2024-07-19 18:17:29 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+pip install requests
+pip install argparse
+pip install colorama
+pip install threading
+echo "Başarıyla kütüphaneler indirildi artık python3 dizinfinder.py yazarak aracı calıştırabilirsiniz"
